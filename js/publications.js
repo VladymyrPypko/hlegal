266a6e38773a5e4e85fe368487ed0c0aad5c9b82
VladymyrPypko <zaitm99@gmail.com>
2024-05-10 19:13:47 +0300

    updated publications.html for mobile version

--- js/publications.js
+++ js/publications.js
@@ -1,10 +1,16 @@
 const switchToggle = document.getElementById('switch').addEventListener('change', function(){
 
     const spanToggle = document.querySelector('.news-articles__toggle');
+    const articles = document.querySelector('.articles');
+    const news = document.querySelector('.news');
 
     if(this.checked){
         spanToggle.innerText = "articles";
+        articles.style.display = "grid";
+        news.style.display = "none";
     } else {
         spanToggle.innerText = "news";
+        articles.style.display = "none";
+        news.style.display = "grid";
     }
 });
